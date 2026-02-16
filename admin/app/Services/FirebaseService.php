2ec68c8848d4a6337d69070adde9b48542fbf040
Fariq <24FTT1059@student.pb.edu.bn>
2026-02-16 12:46:52 +0800

    adding new firebase credential

--- admin/app/Services/FirebaseService.php
+++ admin/app/Services/FirebaseService.php
@@ -2,23 +2,42 @@
 
 namespace App\Services;
 
-
 use Kreait\Firebase\Factory;
+use Kreait\Firebase\Auth;
+use Google\Cloud\Firestore\FirestoreClient;
+use Kreait\Firebase\Firestore;
 
 class FirebaseService
 {
-    protected $firestore;
+    protected Firestore $firestore;
+    protected Auth $auth;
+    protected $storage;
 
     public function __construct()
     {
+
         $factory = (new Factory)
-            ->withServiceAccount(base_path('firebase_credentials.json'));
+            ->withServiceAccount(base_path('config/firebase_credentials.json'));
 
         $this->firestore = $factory->createFirestore()->database();
+
+        $this->auth = $factory->createAuth();
+
+        $this->storage = $factory->createStorage();
     }
 
-    public function getFirestore()
+    public function getFirestore(): FirestoreClient
     {
         return $this->firestore;
+    }
+
+    public function getAuth(): Auth
+    {
+        return $this->auth;
+    }
+
+    public function getStorage()
+    {
+        return $this->storage;
     }
 }
